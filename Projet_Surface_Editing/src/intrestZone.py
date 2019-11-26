f66f84a1cf6de5274fbe29b8e9f97b2fdc05edaf
louteranas <louteranas@gmail.com>
2019-11-26 10:54:48 +0100

    done

--- Projet_Surface_Editing/src/intrestZone.py
+++ Projet_Surface_Editing/src/intrestZone.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from mesh import Mesh
 import math
+from itertools import *
 
 
 class IntrestZone:
@@ -8,6 +9,7 @@ class IntrestZone:
         self.originalMesh = mesh 
         self.numberOfPoints = 0
         self.intrestPoints = []
+        self.faces = []
 
     def computeDistance(self, originPoint, endPoint):
         return math.sqrt(pow(originPoint[0] - endPoint[0], 2) +\
@@ -15,10 +17,18 @@ class IntrestZone:
                          pow(originPoint[2] - endPoint[2], 2))
 
     def findPointsBydistance(self, origin, distance):
-        for point in self.originalMesh.points:
+        for index, point in enumerate(self.originalMesh.points):
             if(self.computeDistance(origin, point) < distance):
                 self.intrestPoints.append(point)
+                self.getFacesInInterestZone(index)
                 self.numberOfPoints += 1
+        self.faces = list(set(self.faces))
+
+    def getFacesInInterestZone(self, index){
+        for face in self.originalMesh.facesIndexs:
+            if(index in face):
+                self.faces.append(face)
+    }
 
     def draw(self):
         print(self.numberOfPoints)
