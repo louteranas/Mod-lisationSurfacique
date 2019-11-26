2bcb9351d5243bbfea411d7f108348f0ab210971
Marion Taconné <Marion.Taconne@grenoble-inp.org>
2019-11-26 11:02:24 +0100

    couleur

--- Projet_Surface_Editing/src/affichage.py
+++ Projet_Surface_Editing/src/affichage.py
@@ -6,6 +6,7 @@ from OpenGL.GLU import *
 from mesh import Mesh
 from test_affichage import *
 from itertools import *
+from intrestZone import *
 
 verticies = (
     (1, -1, -1),
@@ -34,10 +35,11 @@ edges = (
     )
 
 
-def object(myMesh, face):
+def object(myMesh, face, color):
     glBegin(GL_LINES)
     face2 = face + (face[0],)
     for vertex in face2:
+        glColor3f(color[0], color[1], color[2]);
         glVertex3fv(myMesh.points[vertex])
     glEnd()
 
@@ -68,7 +70,14 @@ def affichage(myMesh):
         #glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         #Cube()
+        color_red = (1.0, 0.0, 0.0)
+        color_blue = (0.0, 0.0, 1.0)
+        #color_green = (0.0, 1.0, 0.0)
+        zone = IntrestZone(myMesh)
+        zone.findPointsBydistance(myMesh.points[50], 1.5)
         for face in myMesh.facesIndexs:
-            object(myMesh,face)
+            object(myMesh,face, color_blue)
+        for face in zone.faces:
+            object(myMesh,face, color_red)
         pygame.display.flip()
         pygame.time.wait(10)
